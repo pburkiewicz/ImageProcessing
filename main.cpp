b13ef55a1a0ae89a720fe8ed7dc466efbc092b4b
burkiewicz <burkiewicz.patrycja@gmail.com>
2020-05-13 00:05:21 +0200

    CMake file configuration.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
 using namespace cv;
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string filename;
+    std::cout << "Add file name\n";
+    //std::cin >> filename;
+    Mat x = imread("/home/student/Desktop/circles.png");
+    std::cout << x.type()<< " " << x.channels();
+
+    imshow("image", x);
+    waitKey();
     return 0;
 }
