281247761478a3d2fe9993a3546cf6c07684d21a
burkiewicz <burkiewicz.patrycja@gmail.com>
2020-05-14 19:33:40 +0200

    Created class to manage operations on image.

---
+++ Headers/ImageProcessor.hpp
@@ -0,0 +1,26 @@
+//
+// Created by student on 13.05.2020.
+//
+
+#ifndef IMAGEPROCESSING_IMAGEPROCESSOR_HPP
+#define IMAGEPROCESSING_IMAGEPROCESSOR_HPP
+#include "Image.hpp"
+#include "KernelGenerator.hpp"
+#include <iostream>
+#warning delete iostream include
+
+class ImageProcessor {
+protected:
+    Kernel kernel;
+public:
+    ImageProcessor(size_t radius) : kernel(std::move(KernelGenerator::generate(radius))) {}
+    ImageProcessor(Kernel const& kernel_) : kernel(kernel_) {}
+    ImageProcessor(Kernel&& kernel_) : kernel(std::move(kernel_)) {}
+    virtual ~ImageProcessor() = default;
+
+protected:
+    static auto& getData(Image& i) {return i.data;}
+};
+
+
+#endif //IMAGEPROCESSING_IMAGEPROCESSOR_HPP
