4a94c883b637a65d7127695dda86d328cc6aa1a3
burkiewicz <burkiewicz.patrycja@gmail.com>
2020-05-19 00:01:33 +0200

    Erosion with image borders.

--- Headers/Erosion.hpp
+++ Headers/Erosion.hpp
@@ -8,35 +8,51 @@
 #include "ImageProcessor.hpp"
 #include "KernelGenerator.hpp"
 #include <iostream>
+
 #warning remove include iostream
 
 class Erosion : public ImageProcessor {
 public:
     Erosion(size_t radius) : ImageProcessor(radius) {}
-    Erosion(Kernel const& kernel_) : ImageProcessor(kernel_) {}
-    Erosion(Kernel&& kernel_) : ImageProcessor(std::move(kernel_)) {}
 
-    Image& compute(Image& image) const noexcept { return compute(image, kernel); }
+    Erosion(Kernel const &kernel_) : ImageProcessor(kernel_) {}
 
-    static Image& compute(Image& image, Kernel const& k) noexcept {
+    Erosion(Kernel &&kernel_) : ImageProcessor(std::move(kernel_)) {}
+
+    Image &compute(Image &image) const noexcept { return compute(image, kernel); }
+
+    static Image &compute(Image &image, Kernel const &k) noexcept {
         std::cout << "computing Erosion of image\n";
         auto const height = image.getHeigth();
         auto const width = image.getWidth();
-        auto& data = getData(image);
+        auto &data = getData(image);
         auto const sqrtKernel = static_cast<size_t>(sqrt(k.data.size()));
-        size_t const midKernel = k.data.size()/2;
+        size_t const midKernel = k.data.size() / 2;
         auto mid = midKernel % sqrtKernel;
         auto dataCopy = data;
-        if(1 == image.getChannels()){
-            for(auto row = mid ; row < height-mid; ++row){
-                for(auto col = mid ; col < width-mid; ++col ){
-                    for (int i = 0; i < sqrtKernel && k.data[midKernel] == data[row*width+col] ; ++i) {
-                        for (int j =0; j < sqrtKernel; ++j) {
-                            if(k.data[abs(i)*sqrtKernel + j] == 255 && k.data[abs(i)*sqrtKernel + j]  != data[(i +row) * width + col + j] ){
-                                dataCopy[row*width+col] = 0;
-                                break;
+
+        int sK2 = static_cast<int>(sqrtKernel / 2);
+
+        if (1 == image.getChannels()) {
+            for (auto row = 0; row < height; ++row) {
+                for (auto col = 0; col < width; ++col) {
+                    if(row == 8  && col == 4){
+                    for (int i = -sK2; i <= sK2 && k.data[midKernel] == data[row * width + col]; ++i) {
+                        int const ind = height - row - i;
+                        if ((row - (-i) >= 0) && ind > 0) {
+                            for (int j = -sK2; j <= sK2; ++j) {
+                                int const ind2 = width - col - j;
+                                if (col - (-j) >= 0 && ind2 > 0) {
+                                    if (k.data[(i + sK2) * sqrtKernel + j + sK2] == 255 &&
+                                        k.data[(i + sK2) * sqrtKernel + j + sK2] !=
+                                        data[(i + row) * width + col + j]) {
+                                        dataCopy[row * width + col] = 0;
+                                        break;
+                                    }
+                                }
                             }
                         }
+                    }
                     }
                 }
             }
--- main.cpp
+++ main.cpp
@@ -3,25 +3,28 @@
 #include <opencv2/highgui.hpp>
 #include <string>
 #include <iostream>
+#include <ImageEntropy.hpp>
 
 
 using namespace std::string_literals;
 
 int main() {
 //    auto const path = "/home/student/Pobrane/nowe.bmp"s;
-    auto const path = "/home/student/Desktop/circles.png"s;
+//    auto const path = "/home/student/Desktop/circles.png"s;
+    auto const path = "/home/student/Pobrane/wzorek.bmp"s;
     auto im = Image::factory(std::move(path), "infile"s).value_or(Image());
-//    im.display();
+    im.display();
     cv::Mat mat =  cv::imread(path, 0);
-    cv::erode(mat,mat,cv::getStructuringElement(2,cv::Size(3,3),cv::Point(1,1)));
-    cv::resize(mat, mat, cv::Size(), 2, 2);
+
+    cv::erode(mat,mat,cv::getStructuringElement(2,cv::Size(5,5),cv::Point(2,2)));
+    cv::resize(mat, mat, cv::Size(), 10, 10);
     cv::imshow("name", mat);
     im.display();
     Image out(im, "outfile"s );
-//    out.display();
-    Erosion opening(1);
-    opening.compute(out);
+    Erosion erosion(2);
+    erosion.compute(out);
     out.display();
+
     cv::waitKey(0);
     return 0;
 }
